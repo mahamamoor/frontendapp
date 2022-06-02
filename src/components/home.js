5378ad30ac4010bd752166cf9987ccf061ceed56
Maha Mamoor <mamoorm@gmail.com>
2022-06-02 19:13:32 -0400

    my slideshow on the homepage works. now I need to add text to it.

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,15 @@
 import './css/home.css'
 import {useState, useEffect, useRef} from 'react';
 
-const colors = ["#0088EE", "#00C49F", "#FFBB2B", "#0088EE", "#00C49F"];
-const stats = [
-  "Since 2009, there have been 274 mass shootings in the United States, resulting in 1536 people shot and killed and 983 people shot and wounded.",
-  "1 in 4 mass shooting victims were children and teens.",
-  "1 in 3 mass shootings involved a shooter that was legally prohibited from possessing firearms at the time of the shooting.",
-  "In 56 percent of mass shootings, the shooter exhibited dangerous warning signs before the shooting.",
-  "Between 2009 and 2020, the 5 deadliest mass shootings all involved assault weapons and/or high-capacity magazines."
-];
-const delay = 2500;
+const colors = ["#0088EE", "#FF0000", "#0088EE", "#FF0000", "#0088EE"];
+// const stats = [
+//   "Since 2009, there have been 274 mass shootings in the United States, resulting in 1536 people shot and killed and 983 people shot and wounded.",
+//   "1 in 4 mass shooting victims were children and teens.",
+//   "1 in 3 mass shootings involved a shooter that was legally prohibited from possessing firearms at the time of the shooting.",
+//   "In 56 percent of mass shootings, the shooter exhibited dangerous warning signs before the shooting.",
+//   "Between 2009 and 2020, the 5 deadliest mass shootings all involved assault weapons and/or high-capacity magazines."
+// ];
+const delay = 5000;
 
 function Slideshow() {
 
@@ -25,21 +25,21 @@ function Slideshow() {
   useEffect(() => {
     resetTimeout()
     timeoutRef.current = setTimeout(
-    setTimeout(
       () =>
         setIndex((prevIndex) =>
           prevIndex === colors.length - 1 ? 0 : prevIndex + 1
         ),
       delay
-    ));
+    );
 
-    return () => {}
+    return () => {
     resetTimeout();
+    };
   }, [index]);
 
   return (
     <div className="slideshow">
-      <div className="slideshowSlider" style={{transform: `translate(${-index * 100}%, 0, 0)`}}>
+      <div className="slideshowSlider" style={{transform: `translate3d(${-index * 100}%, 0, 0)`}}>
       {colors.map((backgroundColor, index) => (
         <div className="slide" key={index} style={{backgroundColor}}>
         </div>
@@ -55,8 +55,6 @@ function Slideshow() {
   </div>
   )
 }
-
-
 
 const Home = () => {
   return (
