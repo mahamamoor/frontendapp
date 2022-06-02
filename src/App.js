84abd20509392c721f9303eedf84b144beedbf52
Maha Mamoor <mamoorm@gmail.com>
2022-06-01 20:10:10 -0400

    mapped shooting data to frontend successfully

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useState, useEffect} from 'react';
 
-function App() {
+// import components for mass shootings and home page
+import Msa from './components/msa.js';
+// import Home from './components/home.js';
+
+const App = () => {
+  //state for shootings
+  const [msa, setMsa] = useState([]);
+
+  // get request for shootings database
+  useEffect(() => {
+      axios.get(`https://rocky-savannah-90233.herokuapp.com/project3`)
+          .then((response) => {
+              // console.log(response.data.shooting);
+              setMsa(response.data.shooting);
+      });
+  });
+
   return (
-<h1>hi</h1>
+    <>
+    <Msa msa={msa} />
+    </>
   );
 }
 
---
+++ src/components/msa.js
@@ -0,0 +1,32 @@
+// import React from 'react'
+// import './css/msa.css'
+// import {useState} from 'react';
+
+const Msa = (props) => {
+
+  return (
+    <>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+            <h1>hi</h1>
+            {props.msa.map((shooting) => {
+              return (
+                <>
+                <h5 className="card-title">{shooting.case}</h5>
+                <p className="card-text">{shooting.date}</p>
+                <p className="card-text">{shooting.location__1}</p>
+                <p className="card-text">{shooting.summary}</p>
+                </>
+              )
+            })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Msa
